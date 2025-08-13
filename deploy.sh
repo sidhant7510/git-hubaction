6f4568b4aa96f6c5b91ba4549fc2d339dc0e55f3
Ubuntu <ubuntu@ip-172-31-44-230.ap-south-1.compute.internal>
2025-08-13 14:07:44 +0000

    six

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 pkill -f gunicorn || true
-nohup gunicorn -w 4 -b 0.0.0.0:5000 app:app &
+nohup gunicorn -w 4 -b 0.0.0.0:5000 app:app --daemon
 
