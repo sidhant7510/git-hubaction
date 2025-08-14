3f58eb78c25f47ece5bab97e2a3cb861cc5d4abb
root <root@ip-172-31-41-137.ap-south-1.compute.internal>
2025-08-14 06:03:54 +0000

    this is my seven commit

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@ cd /home/ubuntu/git-hubaction
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-pkill -f gunicorn || true
-nohup gunicorn -w 4 -b 0.0.0.0:5000 app:app --daemon
+python app.py runserver 0.0.0.0:5000
 
