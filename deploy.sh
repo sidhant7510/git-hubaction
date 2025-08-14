8f4ba6d2accb6a1d3322232e4758e15c89601a23
Ubuntu <ubuntu@ip-172-31-41-137.ap-south-1.compute.internal>
2025-08-14 07:50:54 +0000

    ten

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 cd /home/ubuntu/git-hubaction
 
+# Ensure venv package is installed
+if ! dpkg -s python3.12-venv >/dev/null 2>&1; then
+    sudo apt update
+    sudo apt install python3.12-venv -y
+fi
+
 # Create and activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
 # Install dependencies
-pip install -r requirements.txt
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 
 # Kill any running instance of this app
 pkill -f "python app.py" || true
 
 # Start Flask on specified port
-nohup python app.py --host=0.0.0.0 --port=5000 --domean
+nohup python3 app.py --host=0.0.0.0 --port=5000 &
 
